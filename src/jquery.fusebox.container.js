80bc91bc72fce846935c0af94e2bf1b6d2322ce8
Joshua Clayton <jclayton@fusionary.com>
2009-06-09 14:26:16 -0400

    Add jQuery UI, initial fix with jQuery UI classes

--- src/jquery.fusebox.container.js
+++ src/jquery.fusebox.container.js
@@ -1,19 +1,41 @@
 (function($) {
   var displayFuseboxContents = function(selector) {
     $(".fusebox-container")
-      .children(".fusebox").hide().end()  // hide all children
-      .find(selector).show().end()        // display current selector
-      .fadeIn("slow")                     // display .fusebox-container
+      .children(".fusebox").hide().end()                    // hide all children
+      .find(".fusebox:has(" + selector + ")").show().end()  // display current selector
+      .fadeIn("slow")                                       // display .fusebox-container
       .css("left", $(window).width()/2 - ($(".fusebox-container").width()/2)); // position correctly
+    correctShadow();
+  };
+  
+  var correctShadow = function() {
+    var sumAttributeValues = function(array) {
+      var sum = 0;
+      $.each(array, function(idx, attribute) {
+        sum += parseFloat(currentFusebox.css(attribute));
+      });
+      return sum;
+    };
+    
+    var currentFusebox = $(".fusebox-container .fusebox:visible"),
+        transformWidth = currentFusebox.width() + sumAttributeValues(["border-left-width", "border-right-width", "padding-left", "padding-right"]),
+        transformHeight = currentFusebox.height() + sumAttributeValues(["border-top-width", "border-bottom-width", "padding-top", "padding-bottom"]);
+    
+    $(".fusebox-container .ui-widget-shadow").css({width: transformWidth, height: transformHeight});
   };
   
   $.fusebox.container = {
     initialize: function() {
       if($(".fusebox-container").length > 0) { return; }
-      $("body").append($("<div>").addClass("fusebox-container"));
+      $("body").append($("<div>").addClass("fusebox-container").css({display: "none"}).append($("<div>").addClass("ui-widget-shadow")));
     },
     append: function(element) {
-      $(".fusebox-container").append(element);
+      var transform = arguments[1] || function(element) { 
+        return $("<div>")
+          .addClass("fusebox ui-widget-content")
+          .append(element);
+      };
+      $(".fusebox-container").append(transform(element));
     },
     show: function(selector) {
       $(document).trigger("beforeShow.fusebox");
