514a34c4d0d7ef46e343bd08b5d8d3601eb73668
Joshua Clayton <jclayton@fusionary.com>
2009-06-11 11:27:13 -0400

    Copy/paste fix

--- jquery/jquery.fusebox.js
+++ jquery/jquery.fusebox.js
@@ -48,3 +48,39 @@
   
   $(document).bind("close.fusebox", $.fusebox.bindings.close);
 })(jQuery);
+(function($) {
+  var displayFuseboxContents = function(selector) {
+    $(".fusebox-container")
+      .children(".fusebox").hide().end()                    // hide all children
+      .find(".fusebox:has(" + selector + ")").show().end()  // display current selector
+      .fadeIn("slow")                                       // display .fusebox-container
+      .css("left", $(window).width()/2 - ($(".fusebox-container").width()/2)); // position correctly
+  };
+  
+  $.fusebox.container = {
+    initialize: function() {
+      if($(".fusebox-container").length > 0) { return; }
+      $("body").append($("<div class='fusebox-container'><div class='ui-widget-shadow'></div></div>"));
+    },
+    append: function(element) {
+      $(".fusebox-container").append($("<div class='fusebox ui-widget-content'>").append(element));
+    },
+    show: function(selector) {
+      $(document).trigger("beforeShow.fusebox");
+      if($(".fusebox-container").is(":visible")) {
+        $(".fusebox-container").fadeOut("slow", function() { 
+          displayFuseboxContents(selector);
+        });
+      } else {
+        $(".fusebox-container").hide();
+        displayFuseboxContents(selector);
+      }
+      $(document).trigger("show.fusebox").trigger("afterShow.fusebox");
+    },
+    hide: function() {
+      $(document).trigger("beforeHide.fusebox");
+      $(".fusebox-container").fadeOut("slow");
+      $(document).trigger("hide.fusebox").trigger("afterHide.fusebox");
+    }
+  };
+})(jQuery);
